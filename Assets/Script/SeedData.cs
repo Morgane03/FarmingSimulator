7e70823fa8eea2f42dfaa23b1e85193dac4b5a10
m v <morgane.voisin@e-artsup.net>
2024-03-26 09:54:12 +0100

    Add new package + asset

---
+++ Assets/Script/SeedData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "SeedData", menuName = "ScriptableObjects/SeedData", order = 1)]
+public class SeedData : ScriptableObject
+{
+    public string seedName;
+    public Sprite seedSprite;
+    public int growthTime;
+
+
+    public int purchasePrice;
+    public int sellPrice;
+}
